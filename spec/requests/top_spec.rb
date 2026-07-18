05663ec348737b9bac19a80c92898d52c4df3e4c
gattsu75a <241723201+yuki-tech-dev@users.noreply.github.com>
2026-07-18 19:31:38 +0900

    test: 共通ヘッダーの表示を検証するテストを追加

--- spec/requests/top_spec.rb
+++ spec/requests/top_spec.rb
@@ -6,5 +6,20 @@ RSpec.describe "Tops", type: :request do
       get "/"
       expect(response).to have_http_status(:success)
     end
+
+    it "ヘッダーにアプリ名が表示されること" do
+      get "/"
+      expect(response.body).to include("RoboCup Supporter")
+    end
+
+    it "ヘッダーにログインが表示されること" do
+      get "/"
+      expect(response.body).to include("ログイン")
+    end
+
+    it "ヘッダーに新規登録が表示されること" do
+      get "/"
+      expect(response.body).to include("新規登録")
+    end
   end
 end
