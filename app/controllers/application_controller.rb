63adb8e08b56ed28018f6bf2cbc516aef38e3a6c
gattsu75a <241723201+yuki-tech-dev@users.noreply.github.com>
2026-07-19 17:32:02 +0900

    feat: 新規ユーザー登録機能（サインアップ）を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :logged_in?, :current_user
+  # before_action :require_login
+  add_flash_types :success, :danger
+
+  def logged_in?
+    !!current_user
+  end
+
+  # def logout
+  #   session[:user_id] = nil
+  #   @current_user = nil
+  # end
+
+  # def current_user
+  #   @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  # end
+
+  private
+
+  # def require_login
+  #   redirect_to login_path, danger: t('defaults.flash_message.require_login') unless logged_in?
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }, confirmation: true, if: -> { new_record? || password.present? }
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, uniqueness: true
 end
