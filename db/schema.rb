030f04abe6e86a6fe2f844fef2fade94d6018b79
gattsu75a <241723201+yuki-tech-dev@users.noreply.github.com>
2026-07-20 23:07:07 +0900

    feat: ログイン・ログアウト機能とremember_me機能を実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_07_18_062045) do
+ActiveRecord::Schema[7.2].define(version: 2026_07_19_010000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_07_18_062045) do
     t.integer "role", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
