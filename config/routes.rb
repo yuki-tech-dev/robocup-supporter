cffa725427d929a735e9d5d79b61871852ab763e
gattsu75a <241723201+yuki-tech-dev@users.noreply.github.com>
2026-07-22 22:17:33 +0900

    feat: 予定登録のルーティング・コントローラーを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "top#index"
   resources :users, only: %i[new create]
+  resources :schedules, only: %i[new create]
 
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
