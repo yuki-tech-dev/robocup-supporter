8d0379afd446977325454ebd67b56d37425ccc8a
gattsu75a <241723201+yuki-tech-dev@users.noreply.github.com>
2026-07-23 22:47:40 +0900

    feat: ログイン後のトップページに月間カレンダーと直近予定一覧を表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,5 +2,11 @@ class TopController < ApplicationController
   skip_before_action :require_login, only: %i[index]
 
   def index
+    return unless logged_in?
+
+    @schedules = Schedule.all
+    @upcoming_schedules = Schedule.where("start_time >= ?", Time.current)
+                                   .order(start_time: :asc)
+                                   .limit(5)
   end
 end
