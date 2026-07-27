4e160864e3dfcef4c9c3a66dd23d9a1fe13e86b1
gattsu75a <241723201+yuki-tech-dev@users.noreply.github.com>
2026-07-27 19:10:16 +0900

    feat: 教材削除機能のルーティング・コントローラーを実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "top#index"
   resources :users, only: %i[new create]
   resources :schedules, only: %i[new create show edit update destroy]
-  resources :materials, only: %i[new create index]
+  resources :materials, only: %i[new create index destroy]
 
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
